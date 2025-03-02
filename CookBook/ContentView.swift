1e012a522390d944305f4773362609b642e8d690
superkookai <weerawut@live.com>
2025-03-02 10:39:21 +0700

    Initial Commit

---
+++ CookBook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CookBook
+//
+//  Created by Weerawut Chaiyasomboon on 02/03/2568.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CookBook/CookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Weerawut Chaiyasomboon on 02/03/2568.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
